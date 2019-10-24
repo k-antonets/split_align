4af9d049fc0d7b5c8f165b5f371ba293f46f3c40
KirA <kirantonez@gmail.com>
2019-10-25 02:08:46 +0300

    add seq splitter

--- main.go
+++ main.go
@@ -12,6 +12,32 @@ var (
 	outdir = flag.String("out", "", "Output directory")
 )
 
+type SeqEntry struct {
+	Name string
+	Len  int
+}
+
+type SeqSplitter []*SeqEntry
+
+func (s SeqSplitter) Cumulate() []int {
+	l := len(s)
+	r := make([]int, l)
+
+	for i, se := range s {
+		if i == 0 {
+			r[i] = 1
+		} else {
+			r[i] = r[i-1] + se.Len - 1
+		}
+	}
+
+	return r
+}
+
+func (s SeqSplitter) Split(seq *fastx.Record) []*fastx.Record {
+	return nil
+}
+
 func main() {
 	flag.Parse()
 
